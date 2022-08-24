61b819b0c53ce0820781eff62e3dc6e80705e2cc
sawan <ak492016gmail.com>
2022-08-24 23:06:23 +0530

    styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
   return (
     <>
       {/* main div */}
-      <div className='h-[50vh] md:h-[70vh] flex justify-center items-center bg-[#e4b12e] w-full '>
-        <h1 className='text-3xl md:text-4xl lg:text-5xl text-white p-3 text-center font-bold'>Let's Launch Your Beta version Super App</h1>
+      <div className='h-[50vh] md:h-[50vh] flex justify-center items-center bg-[#e4b12e] w-full '>
+        <h1 className='homeHeading text-3xl md:text-4xl w-[80%] md:w-[60%] lg:text-5xl text-white p-3 text-center font-bold'>Let's Launch Your Beta version Super App</h1>
       </div>
       {/* main div ends */}
 
@@ -19,7 +19,7 @@ const Home = () => {
 
 
       {/* launch button */}
-      <Link to="landing"><center><button className='text-2xl flex justify-center items-center mt-12 w-[70%]  bg-[#e4b12e] text-white rounded-full px-8 py-3'>Launch <img className='h-[4.5vh] pl-2 ' src={miniRocket} alt={miniRocket}/></button></center></Link>
+      <Link to="landing"><center><button className='text-2xl flex justify-center items-center mt-12 md:w-[40%] w-[70%]  bg-[#e4b12e] text-white rounded-full px-8 py-3'>Launch <img className='h-[4.5vh] pl-2 ' src={miniRocket} alt={miniRocket}/></button></center></Link>
 
     </>
   );
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
   return (
     <>
     <h1 className='text-3xl md:text-4xl lg:text-5xl mt-10 p-3 text-center font-bold'>Bookvala is for the customer of the customer by the customer </h1>
-
+      <center> <h1 className="text-[200px]">😍</h1></center> 
     <h1 className='text-center text-2xl px-2 mt-12 '>Current active users : 100</h1>
     
     {/* <Link to="/"><p className='text-2xl mt-12 w-[70%]  bg-[#e4b12e] text-white rounded-full px-8 py-3 mx-auto text-center'>Go to back</p></Link> */}
